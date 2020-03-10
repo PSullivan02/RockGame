8db0c73748c2993248a439dc8fc71c74025cbdeb
PSullivan02 <56257997+PSullivan02@users.noreply.github.com>
2020-03-10 08:37:19 -0400

    Add files via upload

--- Laser.java
+++ Laser.java
@@ -1,6 +1,23 @@
 
 package rockgamething;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class Laser extends Entity{
+    private static Color COLOR = new Color(0,144,255);
+    private static final int WIDTH = 5;
+    private static final int HEIGHT = 5;
+    private static final int SPEED = 0;
     
+    public Laser(int x, int y, int vy){
+        super(SPEED, x, y, WIDTH, HEIGHT, COLOR);
+        COLOR = Color.LIGHT_GRAY;
+    }
+    
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(COLOR);
+        g.drawOval(super.getX(), super.getY(), super.getWidth(), super.getHeight());
+    }
 }
